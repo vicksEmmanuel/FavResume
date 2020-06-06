d33bed0c10b54667cc784c265477512e584268d6
vicksemmanuel <vicksemmanuel58@gmail.com>
2020-06-06 22:36:55 +0100

    Can generate .docx resume

---
+++ controllers/Document.js
@@ -0,0 +1,43 @@
+var pdf = require('html-pdf');
+var htmlDocx = require('html-docx-js');
+const fs = require('fs');
+
+class Document {
+    static convertToWord(html, filename, callbackSuccess, callbackError) {
+        var docx = htmlDocx.asBlob(html);
+        fs.mkdir(`./public/${filename.substr(0,filename.lastIndexOf('/'))}`, { recursive: true }, (err) => {
+            if (err) {
+                return callbackError(err);
+            }
+            fs.writeFile(`./public/${filename}.docx`, docx, err => {
+                if (err) {
+                    console.log(err);
+                    return callbackError(err);
+                }
+                return callbackSuccess();
+            });
+        });
+    }
+    static convertToPdf(html, filename, callbackSuccess, callbackError) {
+        //setting options for PDF
+        var options = { format: 'A4', timeout: '100000' };
+        pdf.create(html, options).toFile(`./public/${filename}.pdf`, function (err, resp) {
+            if(resp) return callbackSuccess();
+             if (err) return callbackError(err);
+        });
+    }
+    static checkTypeOfDocumentAndCreate (doc, html, filename, callbackSuccess, callbackError) {
+        switch(doc) {
+            case "pdf":
+                this.convertToPdf(html,filename,callbackSuccess,callbackError);
+                return;
+            case "docx":
+                this.convertToWord(html,filename,callbackSuccess,callbackError);
+                return;
+            default:
+                this.convertToPdf(html,filename,callbackSuccess,callbackError);
+        }
+    }
+}
+
+module.exports = Document;
---
+++ server/server.js
@@ -0,0 +1,80 @@
+//dependency required
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+var app = express();
+const port = process.env.PORT || 3000;
+//const request = require('request');
+const showdown = require('showdown');
+const Resume = require('../controllers/Document.js');
+const Template = require('../controllers/Template.js');
+var cors = require('cors')
+
+const converter = new showdown.Converter();
+
+var fs = require('fs');
+var pdf = require('html-pdf');
+
+//setting view engine 
+app.set('view engine', 'hbs')
+
+
+// middleware
+app.use(cors())
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../public"))
+
+
+
+//Routes
+
+
+
+app.get('/', (req, res) => {
+  res.render('app')
+});
+
+
+app.get('/home', (req, res) => {
+  res.render('index')
+});
+
+app.get('/learn', (req, res) => {
+  res.render('learn')
+});
+
+app.post('/generate', (req, res) => {
+  var data = req.body
+
+  //Reads the Base Template from the Views Folder
+  var template = hbs.compile(fs.readFileSync('././views/gen.hbs', 'utf8'));
+
+  //Proccessing the base template with the content
+  var html = template({content: Template.chooseTemplate(data)})
+
+  var filename = `${data.firstname}${data.lastname}${new Date().toLocaleDateString()}`;
+  //create PDF from the above generated html
+  Resume.checkTypeOfDocumentAndCreate(
+    data.type,
+    html,
+    filename,
+    () => {
+      return res.json({filename:filename+`.${data.type}`})
+    },
+    (err) => {
+      return console.log('Error');
+    }
+  );
+
+});
+
+
+
+//listen to voice of God
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {
+  app
+};
