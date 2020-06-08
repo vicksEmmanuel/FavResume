a90f0711f3cfa232f40d0486482a8eb22648eac2
vicksemmanuel <vicksemmanuel58@gmail.com>
2020-06-08 22:10:43 +0100

    Added another resume template

--- controllers/Template.js
+++ controllers/Template.js
@@ -326,11 +326,191 @@ class Template {
 		<div class="ruler-simulator"></div>
 </body>
 `;
-        switch (data.template) {
+		let webpage3 = `<body class="boxed">
+			<div class="res-header">
+					<div class="res-header-left">
+							${data.image? `<img src="${data.image}" class="p-img"/>`:`<span></span>`}
+					</div>
+					<div class="res-header-right">
+							<div class="name">${data.firstname} ${data.lastname}</div>
+							<div class="profession-title">${data.occupation?data.occupation: ''}</div>
+							<div class="profession-little-msg">
+								${data.description?data.description:''}
+							</div>
+					</div>
+			</div>
+			<table cellpadding="10" style="width:100%">
+			<thead>
+					<tr>
+						${Object.keys(data.contact).map(function(key, index) { return ` 
+							<th>${data.contact[key]}</th>`;
+						}).join('')}
+					</tr>
+			</thead>
+			</table>
+			<div class="holder">
+				<div class="pane">
+					<div class="x-holder">
+						<div class="holder-bar">
+							WORK EXPERIENCE
+						</div>
+						<div class="holder-pane">
+							${data.work_experience.items.map(key => {
+								return `<div class="unbar">
+								<div class="unbar-title">
+									<span class="unbar-title-text">${key.title} at ${key.organisation}</span>
+									<div class="unbar-x">
+										<div class="date-x"><i>${key.from} - ${key.to}</i></div>
+										<div class="location-x"><i>${key.location}</i></div>
+									</div>
+								</div>
+								<div class="unbar-points">
+									<i class="base">${key['technology_used'].tools.map(function (item){ return item}).join(', ') }</i>
+									${key['details'].map(item => {
+										return `<div class="base">
+													<span class="ion">&nbsp;•&nbsp;</span>${item}
+												</div>`;
+									}).join('')}
+								</div>
+							</div>`
+							}).join('')}
+						</div>
+					</div>
+					<div class="x-holder">
+						<div class="holder-bar">
+								SKILLS
+						</div>
+						<div class="holder-pane">
+							${data.skills.details.map(key => {
+								return key['items'].map(newKey => {
+									return `&nbsp;&nbsp;&nbsp;<span class="holder-skill">${newKey}</span>`
+								}).join('')
+							})}
+						</div>
+					</div>
+				</div>
+				<div class="pane">
+					<div class="x-holder">
+						<div class="holder-bar">
+								PERSONAL PROJECTS
+						</div>
+						<div class="holder-pane">
+						${data.github_projects.items.map(key => {
+							return `
+							<div class="unbar">
+								<div class="unbar-title">
+									<span class="unbar-title-text">${key.project_name}</span>${ converter.makeHtml(key['tagline']).replace(/<(\/)?p([^>]*)>/g, '') }								</div>
+								<div class="unbar-points">
+									<i class="base">${key["technology_used"].tools.map(techKey => {
+										return `${techKey}`
+									}).join(', ')}</i>
+									${key["description"].map(desKey => {
+										return `<div class="base">
+													<span class="ion">&nbsp;•&nbsp;</span>${desKey}
+												</div>`
+									}).join(', ')}
+								</div>
+							</div>`
+						}).join('')}
+						</div>
+					</div>
+					<div class="x-holder">
+						<div class="holder-bar">
+								OTHER PROJECTS
+						</div>
+						<div class="holder-pane">
+						${data.other_projects.items.map(key => {
+							return `<div class="unbar">
+							<div class="unbar-title">
+								<span class="unbar-title-text">${key.headline}</span>
+							</div>
+							<div class="unbar-points">
+								<i class="base">${key["technology_used"].tools.map(newKey => {
+									return newKey
+								}).join(', ')}</i>
+								${key.points.map(newKey => {
+									return `<div class="base">
+												<span class="ion">&nbsp;•&nbsp;</span>${newKey}
+											</div>`
+								})}
+							</div>
+						</div>`
+						}).join('')}
+						</div>
+					</div>
+				</div>
+				<div class="pane">
+					<div class="x-holder">
+						<div class="holder-bar">
+							EDUCATION
+						</div>
+						<div class="holder-pane">
+							${data.education.schools.map(key => {
+								return `<div class="unbar">
+											<div class="unbar-title">
+												<span class="unbar-title-text">${key.major}, ${key.degree}</span>
+												<div class="unbar-title-text-2">${key.institution}</div>
+												<div class="unbar-x">
+													<div class="date-x"><i>${key.from?key.from:''} - ${key.to?key.to:''}</i></div>
+												</div>
+											</div>
+										</div>`
+							}).join('')}
+						</div>
+					</div>
+					<div class="x-holder">
+						<div class="holder-bar">
+							INVOLVEMENTS
+						</div>
+						<div class="holder-pane">
+							<div class="unbar">
+								<div class="unbar-points">
+									${data.involvement.organizations.map(key => {
+										return `<div class="base">
+										<span class="ion">&nbsp;•&nbsp;</span>${key}.
+									</div>`
+									}).join('')}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="pane">
+					<div class="x-holder">
+					</div>
+					<div class="x-holder">
+						<div class="holder-bar">
+							RESEARCH EXPERIENCE
+						</div>
+						<div class="holder-pane">
+							${data.research_experience.items.map(key => {
+								return `
+								<div class="unbar">
+									<div class="unbar-title">
+										<span class="unbar-title-text-2">${key.title} at ${key.organisation}</span>
+									</div>
+									<div class="unbar-points">
+										<i class="base">${key.from} to ${key.to}</i>
+										${key.points.map(newKey => {
+											return `<div class="base">
+														<span class="ion">&nbsp;•&nbsp;</span>${newKey}
+													</div>`
+										}).join('')}
+									</div>
+								</div>`
+							}).join('')}
+						</div>
+					</div>
+				</div>
+			</div>
+	</body>`;
+		switch (data.template) {
 			case "1": 
 				return webpage1;
 			case "2":
 				return webpage2;
+			case "3":
+				return webpage3;
             default: 
                 return webpage1;
         }
@@ -342,6 +522,8 @@ class Template {
 				return '././views/gen.hbs';
 			case "2":
 				return '././views/gen2.hbs';
+			case "3":
+				return '././views/gen3.hbs';
 			default:
 				return '././views/gen.hbs';
 		}
