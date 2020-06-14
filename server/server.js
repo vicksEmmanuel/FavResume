5cbd53a219e0b1583775e0010c3e876ad8eb30ec
vicksemmanuel <vicksemmanuel58@gmail.com>
2020-06-14 10:56:09 +0100

    Reimplemented attachment file for emails

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
+var timeout = require('connect-timeout')
 var app = express();
 const port = process.env.PORT || 3000;
 const request = require('request');
@@ -20,6 +21,8 @@ app.set('view engine', 'hbs')
 // middleware
 app.use(cors())
 app.use(bodyParser.json());
+app.use(timeout('5s'));
+app.use(haltOnTimedout);
 app.use(express.static(__dirname + "/../public"))
 
 
@@ -41,7 +44,11 @@ app.get('/learn', (req, res) => {
   res.render('learn')
 });
 
-app.post('/generate', (req, res) => {
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
+
+app.post('/generate', timeout('30s'), haltOnTimedout, (req, res) => {
   var data = req.body
 
   //Reads the Base Template from the Views Folder
@@ -66,7 +73,7 @@ app.post('/generate', (req, res) => {
 
 });
 
-app.post('/generate-and-send', (req, res) => {
+app.post('/generate-and-send', timeout('30s'), haltOnTimedout, (req, res) => {
   var data = req.body
   //Reads the Base Template from the Views Folder
   var template = hbs.compile(fs.readFileSync(Template.choosePathToEngine(data), 'utf8'));
